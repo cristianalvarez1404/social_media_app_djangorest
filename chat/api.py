a63c258dd5d42c47e75c47d47cf2799d6211e9e7
cristiann <cristianandres1496@gmail.com>
2026-06-27 19:52:43 -0500

    chat api

--- chat/api.py
+++ chat/api.py
@@ -2,6 +2,7 @@ from django.http import JsonResponse
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
 
 from .models import Conversation, ConversationMessage
+from account.models import User
 
 from .serializers import ConversationSerializer, ConversationDetailSerializer, ConversationMessageSerializer
 
@@ -16,5 +17,41 @@ def conversation_list(request):
 def conversation_details(request, pk):
   conversation = Conversation.objects.filter(users__in=list([request.user])).get(pk=pk)
   serializer = ConversationDetailSerializer(conversation)
+
+  return JsonResponse(serializer.data, safe=False)
+
+@api_view(['GET'])
+def conversation_get_or_create(request, user_pk):
+  user = User.object.get(pk=user_pk)
+  
+  conversations = Conversation.objects.filter(users__in=list([request.user])).filter(users__in=list([user]))
+
+  if conversations.exists():
+    conversation = conversations.first()
+  else:
+    conversation = Conversation.objects.create()
+    conversation.users.add(user, request.user)
+    conversation.save()
+
+  serializer = ConversationDetailSerializer(conversation)
+
+  return JsonResponse(serializer.data, safe=False)
+
+@api_view(['POST'])
+def conversation_send_message(request, pk):
+  conversation = Conversation.objects.filter(users__in=list([request.user])).get(pk=pk)
+
+  for user in conversation.users.all():
+    if user != request.user:
+      sent_to = user
+
+  conversation_message = ConversationMessage.objects.create(
+    conversation=conversation,
+    body=request.data.get('body'),
+    created_by=request.user,
+    sent_to=sent_to 
+  )
+
+  serializer = ConversationMessageSerializer(conversation_message)
 
   return JsonResponse(serializer.data, safe=False)
